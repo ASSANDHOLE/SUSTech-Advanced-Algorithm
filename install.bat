ae0091a20040e4783adeed9e3a4ed9fcbc3ce9e1
An Guangyan <anguangyan@outlook.com>
2022-11-11 00:35:39 +0800

    fix errors in newly added

--- install.bat
+++ install.bat
@@ -1,16 +1,21 @@
+:: Note that this script is not tested on Windows Platforms, and may not work.
+:: If any unexpected errors occur, please try to find a solution on the Internet.
+
 @echo off
 :: Usage: %0 <Generator>; see available generators with "cmake --help"
 :: if no generator is specified, the default generator for this platform will be used
 :: Example: %0 "MinGW Makefiles"
 SETLOCAL
 set generator=%1
-set hasGenerator=0
-if not [%1]==[] set hasGenerator=1
 cd algorithms_impl || exit /b
 :: Check if the 'build' directory exists or not empty
 if exist build rmdir /s /q build
 set cmakeCmd=cmake -S . -B build
-if %hasGenerator%==1 set cmakeCmd=%cmakeCmd% -G "%generator%"
+:: Check if the generator has been quoted
+if not [%generator%]==[] (
+    if not [%generator:~0,1%]==[" set generator="^%generator^%"
+    set cmakeCmd=%cmakeCmd% -G %generator%
+)
 set cmakeCmd=%cmakeCmd% -DCMAKE_BUILD_TYPE=Release
 :: Run CMake
 echo Building algorithms_impl... with %cmakeCmd%
