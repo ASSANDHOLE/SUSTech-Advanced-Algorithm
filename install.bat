0f7f4a62ede5c61295915425f41ccce27e0e2b24
ASSANDHOLE <anguangyan@outlook.com>
2022-11-24 02:16:06 +0800

    fixed and test install.bat && remove 3 .mat file since they are too big

--- install.bat
+++ install.bat
@@ -1,20 +1,26 @@
-:: Note that this script is not tested on Windows Platforms, and may not work.
-:: If any unexpected errors occur, please try to find a solution on the Internet.
-
 @echo off
 :: Usage: %0 <Generator>; see available generators with "cmake --help"
 :: if no generator is specified, the default generator for this platform will be used
 :: Example: %0 "MinGW Makefiles"
 SETLOCAL
 set generator=%1
+set genFirstChar=%generator:~0,1%
+set genLastChar=%generator:~-1%
+set genFirstChar=%genFirstChar:"=+%
+set genLastChar=%genLastChar:"=+%
+set isQuoted=0
+if "%genFirstChar%"=="+" if "%genLastChar%"=="+" (
+     set isQuoted=1
+)
+
 cd algorithms_impl || exit /b
 :: Check if the 'build' directory exists or not empty
-if exist build rmdir /s /q build
-set cmakeCmd=cmake -S . -B build
+if exist build rmdir /s /q build || exit /b
+set "cmakeCmd=cmake -S . -B build"
 :: Check if the generator has been quoted
 if not [%generator%]==[] (
-    if not [%generator:~0,1%]==[" set generator="^%generator^%"
-    set cmakeCmd=%cmakeCmd% -G %generator%
+    if %isQuoted%==0 set generator="%generator%"
+    set "cmakeCmd=%cmakeCmd% -G %generator%"
 )
 set cmakeCmd=%cmakeCmd% -DCMAKE_BUILD_TYPE=Release
 :: Run CMake
